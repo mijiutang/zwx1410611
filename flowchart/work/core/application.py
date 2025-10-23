d1af2cf0fa6cbc21c064a892336414c9051b2eba
mijiutang <129970328+mijiutang@users.noreply.github.com>
2025-10-23 09:07:46 +0800

    1

--- flowchart/work/core/application.py
+++
@@ -1,8 +0,0 @@
-from ui.main_window import MainWindow
-
-class Application:
-    def __init__(self):
-        self.main_window = MainWindow()
-
-    def show(self):
-        self.main_window.show()
--- flowchart/work/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from core.application import Application
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    main_app = Application()
-    main_app.show()
-    sys.exit(app.exec_())
