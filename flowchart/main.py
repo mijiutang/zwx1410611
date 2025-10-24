9843315299d3f6f50d5db92c6fe8e15b8c492dcf
mijiutang <129970328+mijiutang@users.noreply.github.com>
2025-10-24 20:51:19 +0800

    重组文件

--- flowchart/main.py
+++ flowchart/main.py
@@ -6,7 +6,7 @@ from ui.main_window import MainWindow
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     # Define the root directory of the flowchart project
-    ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
     main_window = MainWindow(root_dir=ROOT_DIR)
     main_window.show()
     sys.exit(app.exec())
